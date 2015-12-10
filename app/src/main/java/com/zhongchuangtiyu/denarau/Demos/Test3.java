818f4c443b8f6cb7005c99dde2dcf7f2c12e1a71
Irnmanuel <wangmeng.wiz@foxmail.com>
2015-12-10 18:56:59 +0800

    update

--- app/src/main/java/com/zhongchuangtiyu/denarau/Demos/Test3.java
+++ app/src/main/java/com/zhongchuangtiyu/denarau/Demos/Test3.java
@@ -3,25 +3,127 @@ package com.zhongchuangtiyu.denarau.Demos;
 import android.content.Intent;
 import android.os.Bundle;
 import android.app.Activity;
-import android.widget.TextView;
+import android.view.View;
+import android.widget.Button;
 
+import com.umeng.socialize.bean.SHARE_MEDIA;
+import com.umeng.socialize.bean.SocializeConfig;
+import com.umeng.socialize.controller.UMServiceFactory;
+import com.umeng.socialize.controller.UMSocialService;
+import com.umeng.socialize.media.UMImage;
+import com.umeng.socialize.sso.UMSsoHandler;
+import com.umeng.socialize.weixin.controller.UMWXHandler;
+import com.umeng.socialize.weixin.media.CircleShareContent;
+import com.umeng.socialize.weixin.media.WeiXinShareContent;
 import com.zhongchuangtiyu.denarau.R;
-
-import cn.jpush.android.api.JPushInterface;
 
 public class Test3 extends Activity
 {
-
+    final UMSocialService mController = UMServiceFactory.getUMSocialService("com.umeng.share");
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_test3);
-        TextView textView = (TextView) findViewById(R.id.textView27);
-        Bundle bundle = getIntent().getExtras();
-        String title = bundle.getString(JPushInterface.EXTRA_EXTRA);
-        textView.setText("extra : " + title);
+        Button button = (Button) findViewById(R.id.btnShare);
+
+        configPlatforms();
+        setShareContent();
+
+        mController.setShareContent("友盟社会化组件（SDK）让移动应用快速整合社交分享功能，http://www.umeng.com/social");
+        // 设置分享图片, 参数2为图片的url地址
+        mController.setShareMedia(new UMImage(Test3.this,
+                R.mipmap.icon));
+
+        button.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                mController.getConfig().setPlatforms(SHARE_MEDIA.WEIXIN, SHARE_MEDIA.WEIXIN_CIRCLE);
+                // 是否只有已登录用户才能打开分享选择页
+                mController.openShare(Test3.this, false);
+            }
+        });
 
     }
+    /**
+     * @功能描述 : 添加微信平台分享
+     * @return
+     */
+    private void addWXPlatform() {
+        // 注意：在微信授权的时候，必须传递appSecret
+        // wx967daebe835fbeac是你在微信开发平台注册应用的AppID, 这里需要替换成你注册的AppID
+        String appId = "wxaf828c2ffaaa6f94";
+        String appSecret = "6bb03e9ee3ed10160b2df80eca4c6a4d";
+        // 添加微信平台
+        UMWXHandler wxHandler = new UMWXHandler(Test3.this, appId, appSecret);
+        wxHandler.addToSocialSDK();
 
+        // 支持微信朋友圈
+        UMWXHandler wxCircleHandler = new UMWXHandler(Test3.this, appId, appSecret);
+        wxCircleHandler.setToCircle(true);
+        wxCircleHandler.addToSocialSDK();
+    }
+
+    /**
+     * 配置分享平台参数</br>
+     */
+    private void configPlatforms() {
+
+        // 添加微信、微信朋友圈平台
+        addWXPlatform();
+    }
+
+    /**
+     * 根据不同的平台设置不同的分享内容</br>
+     */
+    private void setShareContent() {
+
+
+        UMImage localImage = new UMImage(Test3.this, R.drawable.umeng_socialize_action_back);
+        UMImage urlImage = new UMImage(Test3.this,
+                "http://www.umeng.com/images/pic/social/integrated_3.png");
+        // UMImage resImage = new UMImage(getActivity(), R.drawable.icon);
+
+
+
+        WeiXinShareContent weixinContent = new WeiXinShareContent();
+        weixinContent
+                .setShareContent("来自友盟社会化组件（SDK）让移动应用快速整合社交分享功能-微信。http://www.umeng.com/social");
+        weixinContent.setTitle("友盟社会化分享组件-微信");
+        weixinContent.setTargetUrl("http://www.umeng.com/social");
+        weixinContent.setShareMedia(urlImage);
+        mController.setShareMedia(weixinContent);
+
+        // 设置朋友圈分享的内容
+        CircleShareContent circleMedia = new CircleShareContent();
+        circleMedia
+                .setShareContent("来自友盟社会化组件（SDK）让移动应用快速整合社交分享功能-朋友圈。http://www.umeng.com/social");
+        circleMedia.setTitle("友盟社会化分享组件-朋友圈");
+        circleMedia.setShareMedia(urlImage);
+        // circleMedia.setShareMedia(uMusic);
+        // circleMedia.setShareMedia(video);
+        circleMedia.setTargetUrl("http://www.umeng.com/social");
+        mController.setShareMedia(circleMedia);
+
+
+
+    }
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        UMSsoHandler ssoHandler = SocializeConfig.getSocializeConfig().getSsoHandler(requestCode);
+        if (ssoHandler != null) {
+            ssoHandler.authorizeCallBack(requestCode, resultCode, data);
+        }
+    }
+
+
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        mController.getConfig().cleanListeners();
+    }
 }
